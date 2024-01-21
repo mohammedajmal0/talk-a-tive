f7301c92d1a806eb804a85b67fa43d9ae016d9e0
mohammedajmal0 <mohammedajmal007.0@gmail.com>
2024-01-21 21:23:27 +0530

    Models created

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express=require('express');
 const dotenv=require('dotenv');
+const { chats } = require('./data/data');
 dotenv.config();
 const app=express()
 const port=process.env.PORT
 app.listen(port,(console.log(`server started on port ${port}`)));
 app.get("/",(req,res)=>{
     res.send("hello")
+})
+app.get("/api/chat",(req,res)=>{
+    res.send(chats)
 })
