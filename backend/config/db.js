34fec3eae4410ae32d88e261e9107afd9c1aa3ba
mohammedajmal0 <mohammedajmal007.0@gmail.com>
2024-01-23 00:56:41 +0530

    signup and login in backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 const connectDb=async()=>{
     try{
         const conn=await mongoose.connect(process.env.MONGO_URI,{
-            autoIndex:true
+            autoIndex:true,
         });
         console.log('connected to db');
     }
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express=require('express');
 const dotenv=require('dotenv');
 const { chats } = require('./data/data');
 const connectDb = require('./config/db');
+const {notFound,errorHandler} = require('./middlewares/errorMiddleware')
 const color=require('colors')
+const userRoutes=require('./routes/userRoutes');
 dotenv.config();
 connectDb();
 const app=express()
+app.use(express.json()) // to accept json data
 const port=process.env.PORT
 app.listen(port,(console.log(`server started on port ${port}`)));
 app.get("/",(req,res)=>{
@@ -14,3 +17,7 @@ app.get("/",(req,res)=>{
 app.get("/api/chat",(req,res)=>{
     res.send(chats)
 })
+
+app.use(notFound);
+app.use(errorHandler)
+app.use('/api/user',userRoutes);
